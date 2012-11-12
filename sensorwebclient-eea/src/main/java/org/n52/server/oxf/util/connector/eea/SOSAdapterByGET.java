afc877a5114e7d0ffdf2d907edbb5531c2a36afe
ridoo <h.bredel@52north.org>
2012-11-12 12:58:13 +0000

    switch to http commons 4

--- sensorwebclient-eea/src/main/java/org/n52/server/oxf/util/connector/eea/SOSAdapterByGET.java
+++ sensorwebclient-eea/src/main/java/org/n52/server/oxf/util/connector/eea/SOSAdapterByGET.java
@@ -32,7 +32,7 @@ import java.io.InputStreamReader;
 import net.opengis.ows.x11.ExceptionReportDocument;
 import net.opengis.ows.x11.ExceptionType;
 
-import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.HttpEntity;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.n52.oxf.OXFException;
@@ -44,7 +44,8 @@ import org.n52.oxf.ows.capabilities.OnlineResource;
 import org.n52.oxf.ows.capabilities.Operation;
 import org.n52.oxf.sos.adapter.ISOSRequestBuilder;
 import org.n52.oxf.sos.adapter.SOSAdapter;
-import org.n52.oxf.util.IOHelper;
+import org.n52.oxf.util.web.HttpClientException;
+import org.n52.oxf.util.web.SimpleHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,6 +53,8 @@ public class SOSAdapterByGET extends SOSAdapter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SOSAdapterByGET.class);
 
+    private SimpleHttpClient httpClient = new SimpleHttpClient();
+    
     /**
      * Creates an adapter to connect SOS with GET binding as specified by 52n ArcGIS SOS SOE. <br>
      * <br>
@@ -98,11 +101,8 @@ public class SOSAdapterByGET extends SOSAdapter {
             }
             String serviceUrl = getFirstDcpOnlineResourceForGET(operation).getHref();
             serviceUrl = fixServiceUrl(operation, serviceUrl);
-            LOGGER.debug("Built GET request string: {}", requestString);
-            GetMethod method = new GetMethod(serviceUrl);
-            method.setQueryString(requestString);
-            responseStream = IOHelper.execute(method).getResponseBodyAsStream();
-            String responseString = inputStreamToString(responseStream);
+            HttpEntity responseEntity = httpClient.executeGet(serviceUrl + "?" + requestString);
+            String responseString = inputStreamToString(responseEntity.getContent());
             XmlObject response = parseToXmlObject(responseString);
             OperationResult result = new OperationResult(response.newInputStream(), parameters, requestString);
             checkForExceptionReport(result, response);
@@ -113,6 +113,9 @@ public class SOSAdapterByGET extends SOSAdapter {
         }
         catch (XmlException e) {
             throw new OXFException("Could not parse response to XML.", e);
+        }
+        catch (HttpClientException e) {
+            throw new OXFException("Could not send request.", e);
         }
         finally {
             try {
