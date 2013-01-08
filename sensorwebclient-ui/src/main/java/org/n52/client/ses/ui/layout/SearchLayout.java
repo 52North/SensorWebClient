df49377bbb8c28d98d7daa3d28bc10d8c4e4177e
ridoo <h.bredel@52north.org>
2013-01-08 07:00:30 +0000

    move layouts to ui package

--- sensorwebclient-ui/src/main/java/org/n52/client/ses/ui/layout/SearchLayout.java
+++ sensorwebclient-ui/src/main/java/org/n52/client/ses/ui/layout/SearchLayout.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
  * visit the Free Software Foundation web page, http://www.fsf.org.
  */
-package org.n52.client.ses.layout;
+package org.n52.client.ses.ui.layout;
 
 import static org.n52.client.ses.i18n.I18NStringsAccessor.i18n;
 
