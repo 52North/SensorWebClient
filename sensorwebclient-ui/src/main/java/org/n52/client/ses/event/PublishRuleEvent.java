6c9257b5fe61a5772fad1b83f2a518731a2cae16
ridoo <h.bredel@52north.org>
2013-01-30 16:52:36 +0000

    speaking parameters cleanup

--- sensorwebclient-ui/src/main/java/org/n52/client/ses/event/PublishRuleEvent.java
+++ sensorwebclient-ui/src/main/java/org/n52/client/ses/event/PublishRuleEvent.java
@@ -21,86 +21,48 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
  * visit the Free Software Foundation web page, http://www.fsf.org.
  */
+
 package org.n52.client.ses.event;
 
 import org.eesgmbh.gimv.client.event.FilteredDispatchGwtEvent;
 import org.n52.client.ses.event.handler.PublishRuleEventHandler;
 
-/**
- * The Class PublishRuleEvent.
- * 
- * @author <a href="mailto:osmanov@52north.org">Artur Osmanov</a>
- */
 public class PublishRuleEvent extends FilteredDispatchGwtEvent<PublishRuleEventHandler> {
 
-    /** The TYPE. */
     public static Type<PublishRuleEventHandler> TYPE = new Type<PublishRuleEventHandler>();
 
-    /** The ruleName. */
     private String ruleName;
 
-    /** The value. */
-    private boolean value;
-    
-    /** The role */
+    private boolean published;
+
     private String role;
 
-    /**
-     * Instantiates a new publish rule event.
-     * 
-     * @param ruleName 
-     * @param value 
-     * @param role 
-     * @param blockedHandlers
-     *            the blocked handlers
-     */
-    public PublishRuleEvent(String ruleName, boolean value, String role, PublishRuleEventHandler... blockedHandlers) {
+    public PublishRuleEvent(String ruleName, boolean published, String role, PublishRuleEventHandler... blockedHandlers) {
         super(blockedHandlers);
         this.ruleName = ruleName;
-        this.value = value;
+        this.published = published;
         this.role = role;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eesgmbh.gimv.client.event.FilteredDispatchGwtEvent#onDispatch(com
-     * .google.gwt.event.shared.EventHandler)
-     */
     @Override
     protected void onDispatch(PublishRuleEventHandler handler) {
         handler.onPublish(this);
 
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.google.gwt.event.shared.GwtEvent#getAssociatedType()
-     */
     @Override
     public com.google.gwt.event.shared.GwtEvent.Type<PublishRuleEventHandler> getAssociatedType() {
         return TYPE;
     }
 
-    /**
-     * @return {@link String}
-     */
     public String getRuleName() {
         return this.ruleName;
     }
 
-    /**
-     * @return {@link Boolean}
-     */
-    public boolean isValue() {
-        return this.value;
+    public boolean isPublished() {
+        return this.published;
     }
 
-    /**
-     * @return {@link String}
-     */
     public String getRole() {
         return this.role;
     }
