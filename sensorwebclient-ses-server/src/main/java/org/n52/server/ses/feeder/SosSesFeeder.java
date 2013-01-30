e106d111dd4661731c7bbf3ccb5350fb53236403
ridoo <h.bredel@52north.org>
2013-01-30 16:47:58 +0000

    refactor and cleanup documentation

--- sensorwebclient-ses-server/src/main/java/org/n52/server/ses/feeder/SosSesFeeder.java
+++ sensorwebclient-ses-server/src/main/java/org/n52/server/ses/feeder/SosSesFeeder.java
@@ -1,6 +1,9 @@
+
 package org.n52.server.ses.feeder;
 
 import static org.n52.server.ses.feeder.FeederConfig.getFeederConfig;
+import static org.n52.server.ses.feeder.util.DatabaseAccess.decreaseUsageCount;
+import static org.n52.server.ses.feeder.util.DatabaseAccess.increaseUsageCount;
 
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -16,7 +19,7 @@ import net.opengis.sensorML.x101.SensorMLDocument;
 import org.n52.oxf.ows.ExceptionReport;
 import org.n52.server.ses.feeder.connector.SESConnector;
 import org.n52.server.ses.feeder.connector.SOSConnector;
-import org.n52.server.ses.feeder.task.ObservationsTask;
+import org.n52.server.ses.feeder.task.GetObservationsTask;
 import org.n52.server.ses.feeder.util.DatabaseAccess;
 import org.n52.server.ses.hibernate.HibernateUtil;
 import org.n52.shared.serializable.pojos.TimeseriesMetadata;
@@ -29,136 +32,154 @@ public class SosSesFeeder {
     private static final Logger LOGGER = LoggerFactory.getLogger(SosSesFeeder.class);
 
     private Map<String, SOSConnector> sosConnections = new HashMap<String, SOSConnector>();
-    
-    private ObservationsTask obsTask;
-    
+
+    private GetObservationsTask observationTask;
+
     private static SosSesFeeder instance;
-    
+
     public static boolean active = true;
-    
-    /**
-     * List of currently feeding sensors (used to prohibit double feeding)
-     * <br>
-     * We use ONLY the <b>procedureId</b> of the sensor!
-     * <br> 
-     * TODO make config parameter of initial capacity and capacity increment
-     * @deprecated we want to register new timeseries to feed by interface
-     */
-    @Deprecated
-    private static Vector<String> CURRENTLY_FEEDED_SENSORS = new Vector<String>();
-    
+
     private Timer timer = new Timer("Feeder-Timer");
 
     /**
-     * @throws IllegalStateException if feeder could not be initialized from config file. 
+     * Creates the feeder singleton instance if it was not created before.<br>
+     * <br>
+     * The feeder is not started automatically. Use {@link #startFeeding()} or {@link #stopFeeding()} to
+     * control feeding activity or create the instance via {@link SosSesFeeder#createSosSesFeeder(boolean)}.
+     * <br>
+     * <br>
+     * Uses {@link SosSesFeeder#getSosSesFeederInstance()} but without returning the instance.
      */
-    private SosSesFeeder() {
-    	
-    	// initialize feeder configurations
-    	FeederConfig.getFeederConfig();
+    public static void createSosSesFeeder() {
+        createSosSesFeeder(false);
+    }
 
-        // start description timer task
-        this.obsTask = new ObservationsTask(CURRENTLY_FEEDED_SENSORS);
-        
-        // start the tasks
-        startFeeding();
+    /**
+     * Creates the feeder singleton instance if it was not created before.
+     * 
+     * @param startAutomatically
+     *        if the feeder shall start feeding, once it has been created.
+     */
+    public static void createSosSesFeeder(boolean startAutomatically) {
+        if (startAutomatically) {
+            getSosSesFeederInstance().startFeeding();
+        }
+        else {
+            getSosSesFeederInstance();
+        }
     }
-    
-    public static SosSesFeeder getInst() {
-		if (instance == null) {
-			instance = new SosSesFeeder();
-		}
-		return instance;
-	}
-    
-    private void startFeeding() {
+
+    public static SosSesFeeder getSosSesFeederInstance() {
+        if (instance == null) {
+            instance = new SosSesFeeder();
+        }
+        return instance;
+    }
+
+    protected SosSesFeeder() {
+        // singleton, keep private but testable
+    }
+
+    public void startFeeding() {
         LOGGER.info("Start feeding registered timeseries to SES.");
-        this.timer.schedule(this.obsTask, 2000, getFeederConfig().getObsTime());
+        long elapse = getFeederConfig().getElapseTimeOfGetObservationsUpdate();
+        timer.schedule(new GetObservationsTask(), 2000, elapse);
     }
-    
+
+    public void stopFeeding() {
+        timer.cancel();
+        LOGGER.info("Feeding stopped.");
+    }
+
+    public void enableTimeseriesForFeeding(TimeseriesFeed timeseriesFeed) {
+        TimeseriesMetadata timeseriesMetadata = timeseriesFeed.getTimeseriesMetadata();
+        if (timeseriesIsAlreadyBeingFeeded(timeseriesMetadata)) {
+            increaseUsageCount(timeseriesFeed);
+        }
+        else {
+            saveNewTimeseriesFeed(timeseriesFeed);
+        }
+    }
+
+    boolean timeseriesIsAlreadyBeingFeeded(TimeseriesMetadata timeseriesMetadata) {
+        return DatabaseAccess.isKnownTimeseriesFeed(timeseriesMetadata);
+    }
+
+    private void saveNewTimeseriesFeed(TimeseriesFeed timeseriesFeed) {
+        try {
+            SensorMLDocument sensorML = getSensorMLfromSos(timeseriesFeed.getTimeseriesMetadata());
+
+            SESConnector sesConnector = new SESConnector();
+
+            // TODO do we have to exchange unique ID in sensorML?
+
+            String publishedId = sesConnector.registerPublisher(sensorML);
+            timeseriesFeed.setSesId(publishedId);
+            DatabaseAccess.saveTimeseriesFeed(timeseriesFeed);
+        }
+        catch (ExceptionReport e) {
+            LOGGER.error("Error while register sensor in SES, ", e);
+        }
+    }
+
+    private SensorMLDocument getSensorMLfromSos(TimeseriesMetadata timeseriesMetadata) {
+        SOSConnector sosConnector = getSosConnector(timeseriesMetadata);
+        return sosConnector.getSensorML(timeseriesMetadata);
+    }
+
+    private SOSConnector getSosConnector(TimeseriesMetadata timeseriesMetadata) {
+        String serviceUrl = timeseriesMetadata.getServiceUrl();
+        SOSConnector sosConnector = sosConnections.get(serviceUrl);
+        if ( !sosConnections.containsKey(serviceUrl)) {
+            sosConnector = new SOSConnector(serviceUrl);
+            sosConnections.put(serviceUrl, sosConnector);
+        }
+        return sosConnector;
+    }
+
+    public void decreaseSubscriptionCount(TimeseriesFeed timeseriesFeed) {
+        decreaseUsageCount(timeseriesFeed);
+    }
+
     @Override
     public void finalize() {
         try {
-        	// FIXME should be removed during code review before next release
+            // FIXME should be removed during code review before next release
             active = false;
-            this.obsTask.stopObservationFeeds();
+            this.observationTask.stopObservationFeeds();
             // wait until ObsTask is finished
-            while (this.obsTask.isActive()) {
-                LOGGER.debug("ObservationTask is active : " + obsTask.isActive());
+            while (this.observationTask.isActive()) {
+                LOGGER.debug("ObservationTask is active : " + observationTask.isActive());
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e1) {
-                	LOGGER.error("Error during destroy of SosSesFeeder servlet.", e1);
                 }
-            } 
-            
+                catch (InterruptedException e1) {
+                    LOGGER.error("Error during destroy of SosSesFeeder servlet.", e1);
+                }
+            }
+
             stopFeeding();
-            
+
             HibernateUtil.getSessionFactory().close();
 
-            // This manually deregisters JDBC driver, which prevents Tomcat 7 from complaining about memory leaks wrto this class
+            // This manually deregisters JDBC driver, which prevents Tomcat 7 from complaining about memory
+            // leaks wrto this class
             Enumeration<Driver> drivers = DriverManager.getDrivers();
             while (drivers.hasMoreElements()) {
                 Driver driver = drivers.nextElement();
                 try {
                     DriverManager.deregisterDriver(driver);
                     LOGGER.debug(String.format("deregistering jdbc driver: %s", driver));
-                } catch (SQLException e) {
+                }
+                catch (SQLException e) {
                     LOGGER.error(String.format("Error deregistering driver %s", driver), e);
                 }
             }
-            LOGGER.info("########## Feeder complete stopped ##########");
+            LOGGER.info("########## Feeder cleanup completed ##########");
         }
         catch (IllegalStateException e) {
             LOGGER.debug("Connection already closed, because of shutdown process ...", e);
         }
     }
 
-    public void stopFeeding() {
-        LOGGER.info("Stop feeding registered timeseries to SES.");
-        this.timer.cancel();
-    }
-    
-    public void enableTimeseriesForFeeding(TimeseriesFeed timeseriesFeed) {
-    	// check if sensor already registered 
-        TimeseriesMetadata timeseriesMetadata = timeseriesFeed.getTimeseriesMetadata();
-    	boolean timeseriesIsKnown = DatabaseAccess.isKnownTimeseriesFeed(timeseriesMetadata);
-    	if (!timeseriesIsKnown) {
-    		// get sensorML document from SOS
-    		try {
-    		    SOSConnector sosConnector = getSosConnector(timeseriesMetadata);
-				SensorMLDocument sensorML = sosConnector.getSensorML(timeseriesMetadata);
-				
-				// send sensorML document to SES
-				SESConnector sesConn = new SESConnector();
-				
-                // TODO generate unique id (e.g. from FeedingMetadata) and exchange sensorML id
-                
-				String sesID = sesConn.registerPublisher(sensorML);
-				// save in database
-				timeseriesFeed.setSesId(sesID);
-				DatabaseAccess.saveTimeseriesFeed(timeseriesFeed);
-			} catch (ExceptionReport e) {
-				LOGGER.error("Error while register sensor in SES, ", e);
-			}
-    	} else {
-    		DatabaseAccess.increaseSensorUse(timeseriesFeed);
-    	}
-    }
-
-    SOSConnector getSosConnector(TimeseriesMetadata timeseriesMetadata) {
-        SOSConnector sosConn = null;
-        String serviceUrl = timeseriesMetadata.getServiceUrl();
-        if (!sosConnections.containsKey(serviceUrl)) {
-            sosConnections.put(serviceUrl, new SOSConnector(serviceUrl));
-        }
-        sosConn = sosConnections.get(serviceUrl);
-        return sosConn;
-    }
-    
-    public void disableTimeseriesFeed(TimeseriesFeed timeseriesFeed) {
-    	DatabaseAccess.decreaseSensorUse(timeseriesFeed);
-    }
-
-	
 }
