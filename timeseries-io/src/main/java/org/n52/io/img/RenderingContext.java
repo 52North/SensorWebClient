21b6c724d16f7ca256fc781fbcde2a00021350f7
Henning Bredel <h.bredel@52north.org>
2013-08-20 18:29:45 +0200

    move methods to the right place

--- timeseries-io/src/main/java/org/n52/io/img/RenderingContext.java
+++ timeseries-io/src/main/java/org/n52/io/img/RenderingContext.java
@@ -1,25 +1,41 @@
+
 package org.n52.io.img;
 
+import org.n52.io.IOFactory;
+import org.n52.io.IOHandler;
 import org.n52.io.v1.data.DesignedParameterSet;
+import org.n52.io.v1.data.StyleProperties;
 import org.n52.io.v1.data.TimeseriesMetadataOutput;
 
 public class RenderingContext {
-    
+
     private DesignedParameterSet chartStyleDefinitions;
-    
+
     private TimeseriesMetadataOutput[] timeseriesMetadatas;
-    
+
     // use static constructors
     private RenderingContext(DesignedParameterSet timeseriesStyles, TimeseriesMetadataOutput[] timeseriesMetadatas) {
         this.timeseriesMetadatas = timeseriesMetadatas == null ? new TimeseriesMetadataOutput[0] : timeseriesMetadatas;
         this.chartStyleDefinitions = timeseriesStyles;
     }
-    
+
     public static RenderingContext createEmpty() {
         return new RenderingContext(new DesignedParameterSet(), new TimeseriesMetadataOutput[0]);
     }
-    
-    public static RenderingContext createWith(DesignedParameterSet timeseriesStyles, TimeseriesMetadataOutput ... timeseriesMetadatas ) {
+
+    /**
+     * @param timeseriesStyles
+     *        the style definitions for each timeseries.
+     * @param timeseriesMetadatas
+     *        the metadata for each timeseries.
+     * @throws NullPointerException
+     *         if any of the given arguments is <code>null</code>.
+     * @throws IllegalStateException
+     *         if amount of timeseries described by the given arguments is not in sync.
+     * @return a rendering context to be used by {@link IOFactory} to create an {@link IOHandler}.
+     */
+    public static RenderingContext createContextWith(DesignedParameterSet timeseriesStyles,
+                                                     TimeseriesMetadataOutput... timeseriesMetadatas) {
         if (timeseriesStyles == null || timeseriesMetadatas == null) {
             throw new NullPointerException("Designs and metadatas cannot be null.!");
         }
@@ -36,6 +52,18 @@ public class RenderingContext {
         return new RenderingContext(timeseriesStyles, timeseriesMetadatas);
     }
 
+    public static RenderingContext createContextForSingleTimeseries(TimeseriesMetadataOutput metadata,
+                                                                    StyleProperties style) {
+        DesignedParameterSet parameters = new DesignedParameterSet();
+        parameters.addTimeseriesWithStyleOptions(metadata.getId(), style);
+        return RenderingContext.createContextWith(parameters, metadata);
+    }
+
+    public void setDimensions(int width, int height) {
+        chartStyleDefinitions.setWidth(width);
+        chartStyleDefinitions.setHeight(height);
+    }
+
     public DesignedParameterSet getChartStyleDefinitions() {
         return chartStyleDefinitions;
     }
@@ -43,5 +71,5 @@ public class RenderingContext {
     public TimeseriesMetadataOutput[] getTimeseriesMetadatas() {
         return timeseriesMetadatas;
     }
-    
+
 }
