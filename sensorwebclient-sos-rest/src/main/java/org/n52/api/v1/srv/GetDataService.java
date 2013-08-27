0f65e3f321936f9396d160eb4ba232a8e5bcb446
Henning Bredel <h.bredel@52north.org>
2013-08-27 12:45:34 +0200

    add first/last value to timeseries metadata

--- sensorwebclient-sos-rest/src/main/java/org/n52/api/v1/srv/GetDataService.java
+++ sensorwebclient-sos-rest/src/main/java/org/n52/api/v1/srv/GetDataService.java
@@ -25,6 +25,8 @@
 package org.n52.api.v1.srv;
 
 import static org.n52.io.v1.data.TimeseriesData.newTimeseriesData;
+import static org.n52.shared.serializable.pojos.DesignOptions.createOptionsForGetFirstValue;
+import static org.n52.shared.serializable.pojos.DesignOptions.createOptionsForGetLastValue;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,6 +35,7 @@ import java.util.Map;
 import org.n52.client.service.TimeSeriesDataService;
 import org.n52.io.v1.data.TimeseriesData;
 import org.n52.io.v1.data.TimeseriesDataCollection;
+import org.n52.io.v1.data.TimeseriesValue;
 import org.n52.io.v1.data.UndesignedParameterSet;
 import org.n52.shared.requests.TimeSeriesDataRequest;
 import org.n52.shared.responses.TimeSeriesDataResponse;
@@ -77,10 +80,37 @@ public class GetDataService extends DataService {
             }
         }
         catch (Exception e) {
-            LOGGER.error("Could not get timeseries data for options: " + options, e);
             throw new InternalServerException("Could not get timeseries data for options: " + options, e);
         }
         return timeSeriesResults;
+    }
+    
+    public TimeseriesValue getFirstValue(SosTimeseries timeseries) {
+        TimeseriesProperties properties = createTimeseriesProperties(timeseries.getTimeseriesId());
+        DesignOptions designOptions = createOptionsForGetFirstValue(properties);
+        return performFirstOrLastValueRequest(properties, designOptions);
+    }
+    
+    public TimeseriesValue getLastValue(SosTimeseries timeseries) {
+        TimeseriesProperties properties = createTimeseriesProperties(timeseries.getTimeseriesId());
+        DesignOptions designOptions = createOptionsForGetLastValue(properties);
+        return performFirstOrLastValueRequest(properties, designOptions);
+    }
+
+    private TimeseriesValue performFirstOrLastValueRequest(TimeseriesProperties properties, DesignOptions designOptions) {
+       try {
+           TimeseriesDataCollection dataCollection = prepareTimeseriesResults(properties);
+           dataCollection = performTimeseriesDataRequest(dataCollection, designOptions);
+           TimeseriesValue[] data = dataCollection.getTimeseries(properties.getTimeseriesId()).getValues();
+           if (data.length == 0) {
+               LOGGER.error("Server did not return the first/last value for timeseries '{}'.", properties.getTimeseriesId());
+               return null;
+           } 
+           return data[0];
+       } catch (Exception e) {
+           LOGGER.debug("Could not retrieve first or last value request. Probably not supported.");
+           return null;
+       }
     }
 
     public TimeSeriesDataService getTimeSeriesDataService() {
