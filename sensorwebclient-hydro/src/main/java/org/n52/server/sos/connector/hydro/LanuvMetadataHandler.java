7589462dbd90ca1dcb508ffd45927d3cd95d4806
janschulte <j.schulte@52north.org>
2014-05-22 07:52:51 +0200

    remove redundant code to GetFeatureOfInterestParser

--- sensorwebclient-hydro/src/main/java/org/n52/server/sos/connector/hydro/LanuvMetadataHandler.java
+++ sensorwebclient-hydro/src/main/java/org/n52/server/sos/connector/hydro/LanuvMetadataHandler.java
@@ -48,19 +48,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeoutException;
 
-import javax.xml.namespace.QName;
-
-import net.opengis.gml.x32.FeaturePropertyType;
-import net.opengis.sampling.x20.SFSamplingFeatureDocument;
-import net.opengis.sampling.x20.SFSamplingFeatureType;
-import net.opengis.sos.x20.GetFeatureOfInterestResponseDocument;
-import net.opengis.waterml.x20.MonitoringPointDocument;
-import net.opengis.waterml.x20.MonitoringPointType;
-
-import org.apache.xmlbeans.XmlCursor;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
-import org.n52.io.crs.CRSUtils;
 import org.n52.oxf.OXFException;
 import org.n52.oxf.adapter.OperationResult;
 import org.n52.oxf.adapter.ParameterContainer;
@@ -69,6 +58,7 @@ import org.n52.oxf.ows.capabilities.Operation;
 import org.n52.oxf.sos.capabilities.ObservationOffering;
 import org.n52.server.da.AccessorThreadPool;
 import org.n52.server.da.oxf.OperationAccessor;
+import org.n52.server.parser.GetFeatureOfInterestParser;
 import org.n52.shared.serializable.pojos.sos.Category;
 import org.n52.shared.serializable.pojos.sos.Feature;
 import org.n52.shared.serializable.pojos.sos.Offering;
@@ -81,8 +71,6 @@ import org.n52.shared.serializable.pojos.sos.Station;
 import org.n52.shared.serializable.pojos.sos.TimeseriesParametersLookup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.vividsolutions.jts.geom.Point;
 
 public class LanuvMetadataHandler extends HydroMetadataHandler {
 
@@ -232,74 +220,16 @@ public class LanuvMetadataHandler extends HydroMetadataHandler {
             IOException,
             OXFException {
         int counter;
-        TimeseriesParametersLookup lookup = metadata.getTimeseriesParametersLookup();
         counter = getFoiAccessTasks.size();
-        CRSUtils referenceHelper = createReferencingHelper();
         LOGGER.debug("Sending {} GetFeatureOfInterest requests", counter);
         for (String phenomenonID : getFoiAccessTasks.keySet()) {
             LOGGER.debug("Sending #{} GetFeatureOfInterest request for procedure '{}'", counter--, phenomenonID);
             FutureTask<OperationResult> futureTask = getFoiAccessTasks.get(phenomenonID);
             AccessorThreadPool.execute(futureTask);
             try {
-                OperationResult opRes = futureTask.get(SERVER_TIMEOUT, MILLISECONDS);
-                if (opRes == null) {
-                    LOGGER.error("Get no result for GetFeatureOfInterest with procedure: " + phenomenonID + "!");
-                }
-                GetFeatureOfInterestResponseDocument foiResDoc = getFOIResponseOfOpResult(opRes);
-                String id = null;
-                String label = null;
-                for (FeaturePropertyType featurePropertyType : foiResDoc.getGetFeatureOfInterestResponse().getFeatureMemberArray()) {
-                    Point point = null;
-                    XmlCursor xmlCursor = featurePropertyType.newCursor();
-                    if (xmlCursor.toChild(new QName("http://www.opengis.net/samplingSpatial/2.0",
-                                                    "SF_SpatialSamplingFeature"))) {
-                        SFSamplingFeatureDocument samplingFeature = SFSamplingFeatureDocument.Factory.parse(xmlCursor.getDomNode());
-                        SFSamplingFeatureType sfSamplingFeature = samplingFeature.getSFSamplingFeature();
-                        id = sfSamplingFeature.getIdentifier().getStringValue();
-                        if (sfSamplingFeature.getNameArray().length > 0) {
-                            label = sfSamplingFeature.getNameArray(0).getStringValue();
-                        }
-                        else {
-                            label = id;
-                        }
-                        point = createParsedPoint(sfSamplingFeature, referenceHelper);
-                    }
-                    else if (xmlCursor.toChild(new QName("http://www.opengis.net/waterml/2.0", "MonitoringPoint"))) {
-                        MonitoringPointDocument monitoringPointDoc = MonitoringPointDocument.Factory.parse(xmlCursor.getDomNode());
-                        MonitoringPointType monitoringPoint = monitoringPointDoc.getMonitoringPoint();
-                        id = monitoringPoint.getIdentifier().getStringValue();
-                        if (monitoringPoint.getNameArray().length > 0) {
-                            label = monitoringPoint.getNameArray(0).getStringValue();
-                        }
-                        else {
-                            label = id;
-                        }
-                        point = createParsedPoint(monitoringPoint, referenceHelper);
-                    }
-                    else {
-                        LOGGER.error("Don't find supported feature members in the GetFeatureOfInterest response");
-                    }
-                    if (point == null) {
-                        LOGGER.warn("The foi with ID {} has no valid point", id);
-                    }
-                    else {
-                        // if (metadata.getStations().size() > 10) {
-                        // break;
-                        // }
-                        // add feature
-                        Feature feature = new Feature(id, metadata.getServiceUrl());
-                        feature.setLabel(label);
-                        lookup.addFeature(feature);
-
-                        // create station if not exists
-                        Station station = metadata.getStation(id);
-                        if (station == null) {
-                            station = new Station(id, metadata.getServiceUrl());
-                            station.setLocation(point);
-                            metadata.addStation(station);
-                        }
-                    }
-                }
+                OperationResult opsRes = futureTask.get(SERVER_TIMEOUT, MILLISECONDS);
+                GetFeatureOfInterestParser getFoiParser = new GetFeatureOfInterestParser(opsRes, metadata);
+                getFoiParser.createFeatures();
             }
             catch (TimeoutException e) {
                 LOGGER.error("Timeout occured.", e);
